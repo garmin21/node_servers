863892d72cb6454cec7e552ec29fc18e24241dde
Lj <2180727350@qq.com>
2020-01-27 12:23:52 +0800

    完善登入和注册逻辑

--- Controller/user.js
+++ Controller/user.js
@@ -6,47 +6,61 @@ const { getUserInfo, storeUserInfo } = require("../services/user");
 
 /* 封装注册响应 */
 const reqRegister = async ({ username, password, phone }) => {
-  /* 判断用户名是否存储过 */
-  const result = await getUserInfo(username);
+  /* 异步代码有可能会出错 */
+  try {
+    const result = await getUserInfo(username);
 
-  if (result) {
+    if (result) {
+      return new FailureModel({
+        errCode: 1,
+        message: "用户已存在"
+      });
+    }
+    /* 校验成功后存储数据库 */
+    await storeUserInfo({ username, password, phone });
+
+    return new SucceedModel({
+      errCode: 0,
+      data: "用户注册成功"
+    });
+  } catch (err) {
     return new FailureModel({
-      errCode: 1,
-      message: "用户已存在"
+      errCode: 3,
+      message: "网络错误，重连试试"
     });
   }
-  /* 校验成功后存储数据库 */
-  await storeUserInfo({ username, password, phone });
-
-  return new SucceedModel({
-    errCode: 0,
-    data: "用户注册成功"
-  });
 };
 
 /* 封装登入响应 */
 const reqLogin = async ({ username, password }) => {
   /* 判断用户名是否存储过 */
   /* 去数据库中查找用户是否存在 */
-  const result = await getUserInfo(username);
+  try {
+    const result = await getUserInfo(username);
 
-  if (!result) {
-    return new FailureModel({
-      errCode: 2,
-      message: "用户名不存在"
-    });
-  }
+    if (!result) {
+      return new FailureModel({
+        errCode: 2,
+        message: "用户名不存在"
+      });
+    }
 
-  if (password !== result.password) {
-    return new FailureModel({
-      errCode: 3,
-      message: "两次密码输入不一致"
+    if (password !== result.password) {
+      return new FailureModel({
+        errCode: 3,
+        message: "两次密码输入不一致"
+      });
+    }
+    return new SucceedModel({
+      errCode: 0,
+      data: "登入成功"
     });
+  } catch (error) {
+    return new FailureModel({
+      errCode : 3,
+      message : '网络错误，重连试试'
+    })
   }
-  return new SucceedModel({
-    errCode: 0,
-    data: "登入成功"
-  });
 };
 
 module.exports = {
