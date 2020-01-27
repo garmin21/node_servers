4637705195cc9a50d8efb6b4db0700988c765ad3
Lj <2180727350@qq.com>
2020-01-27 12:35:25 +0800

    完成错误的统一处理

--- middlewares/user.js
+++ middlewares/user.js
@@ -1,34 +1,46 @@
 /**
  * @description 应用级中间件，用于复用代码
  */
+const { FailureModel } = require("../Model");
 
- const userCheck = (req,res,next) => {
-    const { username, password, repPassword, phone } = req.body;
+const userCheck = (req, res, next) => {
+  const { username, password, repPassword, phone } = req.body;
+  /* 判断是否是注册路由 */
+  const isRegister = req.url === "/register";
+  /* 定义一个 响应数据对象，进行统一返回错误的响应 */
+  const message = {};
 
-    const isRegister = req.url === '/register'
-  
-    const userReg = /^\w{5,8}$/; // 用户名必须是数字字母下划线，{5，8}
-    const passReg = /^\w{5,13}$/; // 密码必须是数字字母下划线，{5，13}
-    const phoneReg = /^(0|86|17951)?(13[0-9]|15[012356789]|166|17[3678]|18[0-9]|14[57])[0-9]{8}$/; // 匹配手机号
-  
-    if (!userReg.test(username)) {
-      res.send("用户名必须是数字字母下划线，5到8位");
-      return;
-    }
-    if (!passReg.test(password)) {
-      res.send("密码必须是数字字母下划线，5到13位");
-      return;
-    }
-    if (isRegister && !phoneReg.test(phone)) {
-      res.send("手机号不正确");
-      return;
-    }
-    if (isRegister && repPassword !== password) {
-      res.send("两次输入的密码不一致");
-      return;
-    }
-  
-    next();
- }
+  const userReg = /^\w{5,8}$/; // 用户名必须是数字字母下划线，{5，8}
+  const passReg = /^\w{5,13}$/; // 密码必须是数字字母下划线，{5，13}
+  const phoneReg = /^(0|86|17951)?(13[0-9]|15[012356789]|166|17[3678]|18[0-9]|14[57])[0-9]{8}$/; // 匹配手机号
 
- module.exports = userCheck;
+  if (!userReg.test(username)) {
+    message.userError = "用户名必须是数字字母下划线，5到8位";
+  }
+  if (!passReg.test(password)) {
+    message.passError = "密码必须是数字字母下划线，5到13位";
+  }
+  if (isRegister && !phoneReg.test(phone)) {
+    message.phoneError = "手机号不正确";
+  }
+  if (isRegister && repPassword !== password) {
+    message.repPassError = "两次输入的密码不一致";
+  }
+
+  const len = Object.keys(message).length;
+
+  if (len) {
+    const err = new FailureModel({
+      errCode: 5,
+      message
+    });
+    /* 将错误统一返回 */
+    res.json(err);
+
+    return;
+  }
+
+  next();
+};
+
+module.exports = userCheck;
